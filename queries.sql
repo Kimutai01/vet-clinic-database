4095d85bb758801b227590dce8bbaeb15456e8dd
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-08 15:56:13 +0300

    Aggregate functions and more queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,37 @@ SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name !='Gabumon';
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg<=17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals;
+ROLLBACK;
+
+SELECT * from animals;
+
+BEGIN;
+UPDATE animals SET species ='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species ='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+SELECT * FROM animals
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO delete;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts =0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT COUNT(*) as escape_attempts,neutered FROM animals GROUP BY neutered;
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
