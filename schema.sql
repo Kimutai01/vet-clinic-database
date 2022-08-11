025382fa579f391a4bd2338b254a2e41cf742669
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-11 12:00:35 +0300

    Added foreign key

--- schema.sql
+++ schema.sql
@@ -9,3 +9,28 @@ CREATE TABLE animals (
     weight_kg DECIMAL NOT NULL,
 );
 ALTER TABLE animals ADD species varchar(100);
+
+CREATE TABLE owners (
+    id BIGSERIAL NOT NULL
+    full_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE species (
+    id BIGSERIAL NOT NULL
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+ALTER TABLE animals 
+DROP COLUMN species;
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD CONSTRAINT species_key FOREIGN KEY (species_id) REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT owners_key FOREIGN KEY (owner_id) REFERENCES owners(id)
+ON DELETE CASCADE;
